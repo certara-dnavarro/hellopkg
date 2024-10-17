ceb416a5971d0bd60d4491540b5e888edb9cce61
certara-dnavarro <danielle.navarro@certara.com>
2024-10-17 19:59:04 +1100

    adds minimal documentation

--- R/hello.R
+++ R/hello.R
@@ -1,4 +1,14 @@
 
+
+#' Prints hello to screen
+#'
+#' @param name A character vector of length 1
+#'
+#' @return Returns the hello string
+#' @export
+#'
+#' @description
+#' This is a simple function that nobody asked for.
 hello <- function(name) {
   print(paste0("Hello, ", name, "!"))
 }
